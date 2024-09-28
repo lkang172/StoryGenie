17b17c97865be562825f162ebf3e3aa300458ed0
Lucas Kang <kangzundao@gmail.com>
2024-09-28 01:01:07 -0400

    animations

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -1,10 +1,11 @@
 import Navbar from "./Navbar";
+import "./Home.css";
 
 const Home = () => {
   return (
-    <>
-    <Navbar></Navbar>
-    </>
+    <div className="body">
+      <h1>Home</h1>
+    </div>
   );
 };
 
